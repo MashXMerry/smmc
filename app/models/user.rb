0466cd9acf9f3bc25fe266f2bbe071fa6825c7fd
Mashiyyat DelosSantos <delossantos.mash@gmail.com>
2018-03-28 09:24:08 +0800

    Revised signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :username, uniqueness: { message: "A weab already owns that Username" }
+  validates :username, uniqueness: { message: "A weab already owns that Username" }, length: { minimum: 6 , message: "Username is too short" }
+  validates :firstname, length: { minimum: 6 , message: "Firstname is too short"}
+  validates :lastname, length: { minimum: 6 , message: "Lastname is too short"}
+  validates :email, length: { minimum: 6 , message: "Email is too short"}
 end
